7d5370122dfa4a7e2e1aa8103377ba5fe0dc5b20
Hyunseok Seung <tmdgustjr@gmail.com>
2025-01-26 22:55:57 -0500

    sqrt A inv

--- cifar100/optimizers/mac.py
+++ cifar100/optimizers/mac.py
@@ -160,15 +160,10 @@ class MAC(Optimizer):
 
                         state['A_inv'].sub_(torch.outer(exp_avg, exp_avg).div_(damping + sq_norm))
                         state['A_inv'].div_(damping)
-                        state['aat'] = torch.outer(exp_avg, exp_avg)
 
                     A_inv = state['A_inv']
-                    A_proj = state['aat']
 
-                if layer == self.first_layer:
-                    v = grad_mat @ A_inv
-                else:
-                    v = grad_mat @ A_proj @ A_inv
+                v = grad_mat @ A_inv
 
                 if layer.bias is not None:
                     v = [v[:, :-1], v[:, -1:]]
