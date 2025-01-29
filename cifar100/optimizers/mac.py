d95b964e94b57cf7226e1be8e4e27f301a4cf6a0
Hyunseok Seung <tmdgustjr@gmail.com>
2025-01-29 00:33:23 -0500

    sqrt A inv

--- cifar100/optimizers/mac.py
+++ cifar100/optimizers/mac.py
@@ -158,8 +158,7 @@ class MAC(Optimizer):
                         else:
                             state['A_inv'].copy_(torch.eye(exp_avg.size(0), device=exp_avg.device))
 
-                        state['A_inv'].sub_(sq_norm / (damping + sq_norm))
-                        #state['A_inv'].sub_(torch.outer(exp_avg, exp_avg).div_(damping + sq_norm))
+                        state['A_inv'].sub_(torch.outer(exp_avg, exp_avg).div_(damping + sq_norm))
                         #state['A_inv'].div_(damping)
 
                     A_inv = state['A_inv']
