ce95c28ba36ba346adfe35892a238b99e3311970
Hyunseok Seung <tmdgustjr@gmail.com>
2025-01-29 00:46:07 -0500

    sqrt A inv

--- cifar100/optimizers/mac.py
+++ cifar100/optimizers/mac.py
@@ -153,17 +153,21 @@ class MAC(Optimizer):
                         exp_avg = state['exp_avg'].div(bias_correction)
                         sq_norm = torch.linalg.norm(exp_avg).pow(2)
 
-                        if 'A_inv' not in state:
-                            state['A_inv'] = torch.eye(exp_avg.size(0), device=exp_avg.device)
-                        else:
-                            state['A_inv'].copy_(torch.eye(exp_avg.size(0), device=exp_avg.device))
+                        state['A_inv'] = torch.outer(exp_avg, exp_avg).div_(damping + sq_norm)
+                        state['A_inv'].div_(10.0)
 
-                        state['A_inv'].sub_(torch.outer(exp_avg, exp_avg).div_(damping + sq_norm))
+                        #if 'A_inv' not in state:
+                        #    state['A_inv'] = torch.eye(exp_avg.size(0), device=exp_avg.device)
+                        #else:
+                        #    state['A_inv'].copy_(torch.eye(exp_avg.size(0), device=exp_avg.device))
+
+                        #state['A_inv'].sub_(torch.outer(exp_avg, exp_avg).div_(damping + sq_norm))
                         #state['A_inv'].div_(damping)
 
                     A_inv = state['A_inv']
 
-                v = grad_mat @ A_inv
+                #v = grad_mat @ A_inv
+                v = grad_mat - grad.mat @ A_inv
 
                 if layer.bias is not None:
                     v = [v[:, :-1], v[:, -1:]]
