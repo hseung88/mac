b916290e9f58cfc1c808802bc93f2f42617a3e55
Hyunseok Seung <tmdgustjr@gmail.com>
2025-01-30 12:59:18 -0500

    sqrt A inv

--- cifar100/optimizers/mac.py
+++ cifar100/optimizers/mac.py
@@ -146,19 +146,17 @@ class MAC(Optimizer):
                     if b_updated:
                         bias_correction = 1.0 - (stat_decay ** self.emastep)
                         exp_avg = state['exp_avg'].div(bias_correction)
-                        sq_norm = torch.linalg.norm(exp_avg).pow(2)
+                        #sq_norm = torch.linalg.norm(exp_avg).pow(2)
 
                         if 'A_inv' not in state:
                             state['A_inv'] = torch.eye(exp_avg.size(0), device=exp_avg.device)
+                        #else:
+                        #    state['A_inv'].copy_(torch.eye(exp_avg.size(0), device=exp_avg.device))
 
-                        inv_prev = state['A_inv'].clone()
-                        #state['A_inv'].copy_(torch.eye(exp_avg.size(0), device=exp_avg.device))
+                        inv_a = torch.matmul(state['A_inv'], exp_avg)
+                        state['A_inv'].sub_(torch.outer(inv_a, inv_a).div_(1.0 + torch.dot(exp_avg, inv_a)))
 
-                        #inv_a = torch.matmul(state['A_inv'], exp_avg)
-                        #state['A_inv'].sub_(torch.outer(inv_a, inv_a).div_(1.0 + torch.dot(exp_avg, inv_a)))
-
-                        state['A_inv'] = -torch.outer(exp_avg, exp_avg).div_(damping + sq_norm)
-                        state['A_inv'].add_(inv_prev)
+                        #state['A_inv'].sub_(torch.outer(exp_avg, exp_avg).div_(damping + sq_norm))
                         #state['A_inv'].div_(damping)
 
                     A_inv = state['A_inv']
