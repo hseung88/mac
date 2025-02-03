58afa745ef1e7a0317b4ad5c7b1e95f1ef2ba3f9
Hyunseok Seung <tmdgustjr@gmail.com>
2025-02-02 22:01:41 -0500

    sqrt A inv

--- cifar100/optimizers/mac.py
+++ cifar100/optimizers/mac.py
@@ -155,7 +155,7 @@ class MAC(Optimizer):
                         bias_correction = 1.0 - (stat_decay ** self.emastep)
                         exp_avg = state['exp_avg'].div(bias_correction)
                         #exp_avg_proj = state['exp_avg_proj'].div(bias_correction)
-                        #sq_norm = torch.linalg.norm(exp_avg).pow(2)
+                        sq_norm = torch.linalg.norm(exp_avg).pow(2)
                         #sq_norm_proj = torch.linalg.norm(exp_avg_proj).pow(2)
 
                         if 'A_inv' not in state:
@@ -163,7 +163,7 @@ class MAC(Optimizer):
                         else:
                             state['A_inv'].copy_(torch.eye(exp_avg.size(0), device=exp_avg.device))
 
-                        state['A_inv'].sub_(torch.outer(exp_avg, exp_avg).div_(damping + exp_avg.norm()))
+                        state['A_inv'].sub_(torch.outer(exp_avg, exp_avg).div_(damping + sq_norm))
                         #state['A_inv'].sub_(torch.outer(exp_avg_proj, exp_avg_proj).div_(torch.sqrt(damping + sq_norm_proj)))
                         #state['A_inv'].div_(damping)
 
