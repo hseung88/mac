511f190d77f68f595de3760610123cfba528ec8a
Hyunseok Seung <tmdgustjr@gmail.com>
2025-02-02 23:26:26 -0500

    sqrt A inv

--- cifar100/optimizers/fdadam.py
+++ cifar100/optimizers/fdadam.py
@@ -120,7 +120,7 @@ class FDAdam(Optimizer):
                 # Compute the adaptive step. Here we use the standard AdamW update rule,
                 # replacing exp_avg_sq with effective_second_moment.
                 #denom = (effective_second_moment.sqrt() / math.sqrt(bias_correction2)).add_(eps)
-                denom = (exp_avg_curv.sqrt() / math.sqrt(bias_correction2)).add_(eps)
+                denom = (exp_avg_curv / bias_correction2).add_(eps)
                 step_size = lr / bias_correction1
 
                 # Parameter update
