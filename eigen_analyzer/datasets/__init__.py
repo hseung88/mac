18a07fbc1993f8b8caa232b428aacdb6a609c7e6
Hyunseok Seung <tmdgustjr@gmail.com>
2025-02-12 00:36:38 -0500

    sqrt A inv

--- eigen_analyzer/datasets/__init__.py
+++ eigen_analyzer/datasets/__init__.py
@@ -1,5 +1,6 @@
 from .cifar import CIFAR10DataModule
+from .cifar100 import CIFAR100DataModule
 from .fmnist import FashionMNISTDataModule
 from .tiny_imagenet import TinyImageNetDataModule
 
-__all__ = ['CIFAR10DataModule', 'FashionMNISTDataModule', 'TinyImageNetDataModule']
+__all__ = ['CIFAR10DataModule', 'CIFAR100DataModule', 'FashionMNISTDataModule', 'TinyImageNetDataModule']
