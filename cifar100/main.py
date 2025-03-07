a0962f2318213e4c23c76f150bc7418b5a04582a
Hyunseok Seung <tmdgustjr@gmail.com>
2025-03-07 14:05:06 -0500

    attention layer hadnling

--- cifar100/main.py
+++ cifar100/main.py
@@ -88,12 +88,12 @@ def build_dataset(args):
     trainset = torchvision.datasets.CIFAR100(root='./data', train=True, download=True,
                                             transform=transform_train)
     train_loader = torch.utils.data.DataLoader(trainset, batch_size=args.batchsize, shuffle=True,
-                                               num_workers=4, pin_memory=True)
+                                               num_workers=4, pin_memory=True, drop_last=True)
 
     testset = torchvision.datasets.CIFAR100(root='./data', train=False, download=True,
                                            transform=transform_test)
     test_loader = torch.utils.data.DataLoader(testset, batch_size=args.batchsize, shuffle=False, 
-                                              num_workers=4, pin_memory=True)
+                                              num_workers=4, pin_memory=True, drop_last=True)
 
     return train_loader, test_loader
 
