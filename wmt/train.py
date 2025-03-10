028b376adbebfcfd38e4e9b6b518b0b529ef4b0c
Hyunseok Seung <tmdgustjr@gmail.com>
2025-03-09 20:12:59 -0400

    attention layer hadnling

--- wmt/train.py
+++ wmt/train.py
@@ -55,6 +55,8 @@ from transformers import (
 from transformers.utils import check_min_version, send_example_telemetry
 from transformers.utils.versions import require_version
 
+from optimizers.mac import MAC
+
 # Will error if the minimal version of Transformers is not installed. Remove at your own risks.
 check_min_version("4.50.0.dev0")
 
